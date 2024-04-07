d3d933508f48ef43cb63a7f3d3c30b2f6f5e13c9
KDyulgerov <kdyulgerov.sport@gmail.com>
2024-04-07 12:53:50 +0300

    Updated First Task of the Exercise

---
+++ 01.SerializationAndDeserialization/01.BuildInJSONSupport/01.BuildInJSONSupport/Program.cs
@@ -0,0 +1,30 @@
+﻿using _01.BuildInJSONSupport;
+using System.Text.Json;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        WeatherForecast forecast = new WeatherForecast()
+        {
+            Date = DateTime.Now,
+            Temperature = 32,
+            Summary = "Example Random Summary"
+        };
+
+        string weatherInfo = JsonSerializer.Serialize(forecast);
+
+        Console.WriteLine(weatherInfo);
+
+        string jsonString = File.ReadAllText(Path.Combine(Environment.CurrentDirectory + "/../../../weatherForecast.json"));
+
+        var weatherForecastObj = JsonSerializer.Deserialize<List<WeatherForecast>>(jsonString);
+
+        foreach (var forecastItem in weatherForecastObj)
+        {
+            Console.WriteLine($"Date: {forecastItem.Date.ToShortDateString()}, {Environment.NewLine}" +
+                             $"Temp (C): {forecastItem.Temperature}, {Environment.NewLine}" +
+                             $"Summary: {forecastItem.Summary}");
+        }
+    }
+}
---
+++ 01.SerializationAndDeserialization/01.BuildInJSONSupport/01.BuildInJSONSupport/WeatherForecast.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.BuildInJSONSupport
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int Temperature {  get; set; }
+
+        public string Summary { get; set; }
+
+    }
+}
