b4a73d1d245044e0dbd534b97c907b4623c9f394
KDyulgerov <kdyulgerov.sport@gmail.com>
2024-04-07 23:27:23 +0300

    Updated Second Task of the Exercise

---
+++ 01.SerializationAndDeserialization/02.JSONdotNet/02.JSONdotNet/Product.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02.JSONdotNet
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        public string? Name { get; set; }
+
+        public string? Description { get; set; }
+
+        public double Cost { get; set; }
+
+        public Product(int id, string name, string? description, double cost)
+        {
+            this.Id = id;
+            this.Name = name;
+            this.Description = description;
+            this.Cost = cost;
+
+        }
+
+        public void DisplayProductInfo()
+        {
+            Console.WriteLine($"ID: {Id},{Environment.NewLine}Name: {Name},{Environment.NewLine}Description: {Description},{Environment.NewLine}Cost: {Cost}");
+        }
+    }
+}
---
+++ 01.SerializationAndDeserialization/02.JSONdotNet/02.JSONdotNet/Program.cs
@@ -0,0 +1,34 @@
+﻿using _02.JSONdotNet;
+using Newtonsoft.Json;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Serialization from C# to JSON
+
+        Product pump = new Product(0, "Oil Pump", null, 25.0);
+        Product filter = new Product(0, "Oil Filter", null, 15.0);
+
+        pump.DisplayProductInfo();
+        filter.DisplayProductInfo();
+
+        string firstProductJson = JsonConvert.SerializeObject(pump, Formatting.Indented);
+        Console.WriteLine(firstProductJson);
+
+        string secondProductJson = JsonConvert.SerializeObject(filter, Formatting.Indented);
+        Console.WriteLine(secondProductJson);
+
+        // Serialization from JSON to C#
+
+        string jsonString = File.ReadAllText(Path.Combine(Environment.CurrentDirectory + "/../../../Products.json"));
+
+        var objProducts = JsonConvert.DeserializeObject<Dictionary<string, Product>>(jsonString);
+
+        foreach (var kvp in objProducts)
+        {
+            Product product = kvp.Value;
+            Console.WriteLine($"Product Type: {kvp.Key},{Environment.NewLine}Name: {product.Name},{Environment.NewLine}Cost: {product.Cost}");
+        }
+    }
+}
